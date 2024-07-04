5ea0aa98055ee4f9d0164a5d38f67995a06e1c33
Yatharth2609 <yatharth.mishra2002@gmail.com>
2024-07-04 19:47:09 +0530

    feat: Add new user profile endpoints and functionality

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -19,10 +19,29 @@ router.route("/register").post(
   controller.registerUser
 );
 
-router.route("/login").post(controller.loginUser)
+router.route("/login").post(controller.loginUser);
 
-router.route("/logout").post(verifyJWT, controller.logoutUser)
-router.route("/refresh-token").post(controller.refreshAccessToken)
+router.route("/logout").post(verifyJWT, controller.logoutUser);
+router.route("/refresh-token").post(controller.refreshAccessToken);
+router
+  .route("/change-password")
+  .post(verifyJWT, controller.changeCurrentPassword);
+router.route("/current-user").get(verifyJWT, controller.getCurrentUser);
+router
+  .route("/update-account")
+  .patch(verifyJWT, controller.updateAccountDetails);
 
+router
+  .route("/avatar")
+  .patch(verifyJWT, upload.single("avatar"), controller.updateUserAvatar);
+router
+  .route("/cover-image")
+  .patch(
+    verifyJWT,
+    upload.single("/coverImage"),
+    controller.updateUserCoverImage
+  );
+router.route("/c/:username").get(verifyJWT, controller.getUserChannelProfile);
+router.route("/history").get(verifyJWT, controller.getWatchHistory);
 
 module.exports = router;
